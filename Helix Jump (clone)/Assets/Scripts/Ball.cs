7fbabdfb477d7adf9fc78eb6ff843b352341d586
Hüseyin <103373602+HuseyinOztuna03@users.noreply.github.com>
2022-11-27 00:59:45 +0300

    add splash to ball

--- Helix Jump (clone)/Assets/Scripts/Ball.cs
+++ Helix Jump (clone)/Assets/Scripts/Ball.cs
@@ -7,9 +7,13 @@ public class Ball : MonoBehaviour
     public Rigidbody rb;
     public float jumpForce;
 
+    public GameObject splashPrefab;
+
     private void OnCollisionEnter(Collision collision)
     {
         rb.velocity = Vector3.up * jumpForce;
+        GameObject splash = Instantiate(splashPrefab, transform.position + new Vector3(0, -0.22f, 0), transform.rotation);
+        splash.transform.SetParent(collision.gameObject.transform);
 
         string materialName = collision.gameObject.GetComponent<MeshRenderer>().material.name;
         Debug.Log(materialName);
